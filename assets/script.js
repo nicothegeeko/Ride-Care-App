53acef99fee704cc7a1efbb4414bb4dbac144061
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-03-19 14:23:21 -0700

    Testing the metro API

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,40 @@
-$(document).ready(function() {
 
-	//queryURL for metro API ROUTES
-function busRoute() {
-  var queryRoute = "https://api.metro.net/agencies/lametro/routes/180/";
-    $.get(queryRoute, function(data, success, type) {
-      console.log(data, 'data,!!!!!!!!!!!!!!!!!', success, type)
-    })
-     .success(function(response) {
-      console.log('123412341',response);
-    }).error(function(err) {
-      console.log('lskfjlksdjf', err)
-    })
+  //queryURL for metro API ROUTES
+// function busRoute() {
+  // var queryRoute = "https://api.metro.net/agencies/lametro/routes/180/";
+  //   $.get(queryRoute, function(data, success, type) {
+  //     console.log(data, 'data,!!!!!!!!!!!!!!!!!', success, type)
+  //   })
+  //    .success(function(response) {
+  //     console.log('123412341',response);
+  //   }).error(function(err) {
+  //     console.log('lskfjlksdjf', err)
+  //   })
   
-  
-}
-  
-    
-    $(".dropdown").hide();
+    var queryURL = "https://api.metro.net/agencies/lametro/routes/180/";
 
-    $(".btn").click(function(){
-        $(".container").hide({left: '250px'});
-        $(".dropdown").show();
-        $(".dropdown").animate({left: '250px'});
+    $.ajax ({
+      url: queryURL,
+      method:"GET"
+    }).done(function(response){
+
+      console.log(response);
+      var results = response.data;
     });
+  
 
-    $(".dropdown-toggle").dropdown();
-});
+  
+// $(document).ready(function() {
+    
+//     $(".dropdown").hide();
+
+//     $(".btn").click(function(){
+//         $(".container").hide({left: '250px'});
+//         $(".dropdown").show();
+//         $(".dropdown").animate({left: '250px'});
+//     });
+
+//     $(".dropdown-toggle").dropdown();
+// });
 
 
