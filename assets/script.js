9c679342f27197cf25689cf005d58fba3a559cbd
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-03-19 16:06:03 -0700

    adding recent changes to my branch

--- assets/script.js
+++ assets/script.js
@@ -16,12 +16,13 @@ function busRoute() {
 }
   
     
-    $(".dropdown").hide();
+    $("#dropdown1").hide();
+    $("#dropdown2").hide();
 
     $(".btn").click(function(){
         $(".container").hide({left: '250px'});
-        $(".dropdown").show();
-        $(".dropdown").animate({left: '250px'});
+        $("#soundDropdown").show();
+        $("#soundDropdown").animate({left: '250px'});
     });
 
     $(".dropdown-toggle").dropdown();
