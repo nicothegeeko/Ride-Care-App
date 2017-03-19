178d32db92b0e6ab2fb55bead00695caf696d608
Skyler La <yenla@Skylers-MacBook-Pro.local>
2017-03-18 21:12:16 -0700

    Add firebase.js file

---
+++ assets/firebase.js
@@ -0,0 +1,69 @@
+var config = {
+    apiKey: "AIzaSyBNKDQxtZjk6S7NwtlK0xgNT64ZDh3nx9s",
+    authDomain: "ride-care-7c85d.firebaseapp.com",
+    databaseURL: "https://ride-care-7c85d.firebaseio.com",
+    storageBucket: "ride-care-7c85d.appspot.com",
+    messagingSenderId: "625233184128"
+  };
+  firebase.initializeApp(config);
+
+// Firebase information
+// Public-facing name help_outline Ride-Care
+// Project ID help_outline ride-care-7c85d
+// Web API Key AIzaSyBNKDQxtZjk6S7NwtlK0xgNT64ZDh3nx9s 
+// https://ride-care-7c85d.firebaseio.com/ 
+
+// Create a variable to reference the database
+var database = firebase.database();
+var ref = database.ref("users");
+
+// VERIFY USER INFORMATION
+var userInput = prompt('enter user name: ');
+var passwordInput = prompt('enter password: ')
+
+// var userInput = 'tom'; // used for testing
+// var passwordInput = 'tom';  // used for testing
+
+var userValid = false;
+var passwordValid = false;
+
+var thisUserValid = false;
+var thisPasswordValid = false;
+
+var userOK = false;
+
+ref.on('value', function(snapshot) {
+  snapshot.forEach(function(childSnapshot) {
+    var userData = childSnapshot.val();
+    console.log('user: ', userData.user);
+    console.log('password: ', userData.password)
+    if (userInput === userData.user) {
+      thisUserValid = true;
+      userValid = true;
+      console.log('this user valid?', thisUserValid)
+      if (passwordInput === userData.password) {
+          passwordValid = true;
+          thisPasswordValid = true;
+          console.log('this password valid? ', thisPasswordValid)
+      }
+      else {
+          thisPasswordValid = false;
+          console.log('this password valid? ', thisPasswordValid)
+      }
+    }
+    else {
+      thisUserValid = false;
+      thisPasswordValid = null;
+      console.log('this user valid?', thisUserValid);
+      console.log('this password valid? ', thisPasswordValid)
+    }  
+  });
+    // alert('check user and password now'); 
+    console.log('--------------------------');
+    console.log('userValid: ', userValid);
+    console.log('passwordValid: ', passwordValid);
+    if (userValid && passwordValid) {
+      userOK = true;
+    }
+    console.log('User OK?', userOK);
+});
