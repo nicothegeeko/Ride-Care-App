69f3027c24740903bf87d82cfeb4ea14931342dd
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-03-24 09:20:54 -0700

    trying to resolve merge conflict

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,17 @@
-$(document).ready(function() {
+var config = {
+    apiKey: "AIzaSyBNKDQxtZjk6S7NwtlK0xgNT64ZDh3nx9s",
+    authDomain: "ride-care-7c85d.firebaseapp.com",
+    databaseURL: "https://ride-care-7c85d.firebaseio.com",
+    storageBucket: "ride-care-7c85d.appspot.com",
+    messagingSenderId: "625233184128"
+  };
 
-	//queryURL for metro API ROUTES
-function busRoute() {
-  var queryRoute = "https://api.metro.net/agencies/lametro/routes/180/";
-    $.get(queryRoute, function(data, success, type) {
-      console.log(data, 'data,!!!!!!!!!!!!!!!!!', success, type)
-    })
-     .success(function(response) {
-      console.log('123412341',response);
-    }).error(function(err) {
-      console.log('lskfjlksdjf', err)
-    })
-  
-  
-}
+firebase.initializeApp(config);
 
-if (thisPasswordValid = true thisUserValid = true) {
-          // passwordValid = true;
-          // thisUserValid = true;
-          // thisPasswordValid = true;
-
-          
-        $(".container").hide({left: '250px'});
-        $("#soundDropdown").show();
-        $("#dropdown1").show();
-        $("#dropdown2").show();
-        $("#soundDropdown").animate({left: '250px'});
-        $("#logIn-form").hide();
-
+<<<<<<< HEAD
+  // Create a variable to reference the database
+var database = firebase.database();
+=======
           console.log('this password valid? ', thisPasswordValid)
       }
 
@@ -55,71 +39,108 @@ if (thisPasswordValid = true thisUserValid = true) {
     $("#soundDropdown").hide();
     
     
-
-    // $(".btn").click(function(){
-    //     $(".container").hide({left: '250px'});
-    //     $("#soundDropdown").show();
-    //     $("#dropdown1").show();
-    //     $("#dropdown2").show();
-    //     $("#soundDropdown").animate({left: '250px'});
-    //     $("#logIn-form").hide();
-       
+>>>>>>> 0d7f63f78e1bdcfa4d18dd24de6bf501ff029d21
 
 
-    // });
+// Firebase information
+// Public-facing name help_outline Ride-Care
+// Project ID help_outline ride-care-7c85d
+// Web API Key AIzaSyBNKDQxtZjk6S7NwtlK0xgNT64ZDh3nx9s 
+// https://ride-care-7c85d.firebaseio.com/ 
+$("#sign-in-submit").click(function(event){
+  console.log('----- running isUserValid function -----');
 
-    //$(".btn").click (function load() 
-    //{ 
-        //var mapDiv = document.getElementById("map");
+  var form = document.getElementById('userInputForm');
 
-      //});
+  var userInput = form.elements.username.value;    // Select username entered
+  var passwordInput = form.elements.password.value; // Select password entered
 
-    $(".dropdown-toggle").dropdown();
+  var userOK = false;  // initialize Boolean variable
+  var ref = database.ref("users");
 
-});
-    // AIzaSyCkV6mjFSLSVwZajhV4fWy-76XUHtzEW1E
+  // VERIFY USER INFORMATION
+  // var userInput = prompt('enter user name: '); // used in testing only
+  // var passwordInput = prompt('enter password: ')  // used in testing only
 
-// Initiate Map to display google maps
-      function initMap() {
-        var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13,
-          center: {lat: 34.052235, lng: -118.243683}
-        });
+  // var userInput = 'tom'; // used for testing
+  // var passwordInput = 'tom';  // used for testing
 
-        var infoWindow = new google.maps.InfoWindow({map: map});
+  // initialize Boolean variables
+  var userValid = false;
+  var passwordValid = false;
 
+  var thisUserValid = false;
+  var thisPasswordValid = false;
 
-        // Condition to grab user  geolocation.
-        if (navigator.geolocation) {
-          navigator.geolocation.getCurrentPosition(function(position) {
-            var pos = {
-              lat: position.coords.latitude,
-              lng: position.coords.longitude
-            };
-
-            infoWindow.setPosition(pos);
-            infoWindow.setContent('Location found.');
-            map.setCenter(pos);
-          }, function() {
-            handleLocationError(true, infoWindow, map.getCenter());
-          });
-        } else {
-          // Browser doesn't support Geolocation
-          handleLocationError(false, infoWindow, map.getCenter());
+  if (thisPasswordValid = true);{
+          // passwordValid = true;
+          // thisUserValid = true;
+          // thisPasswordValid = true;
+          
+        $(".container").hide({left: '250px'});
+        $("#soundDropdown").show();
+        $("#dropdown1").show();
+        $("#dropdown2").show();
+        $("#soundDropdown").animate({left: '250px'});
+        $("#logIn-form").remove();
+          console.log('this password valid? ', thisPasswordValid)
         }
 
-        // if browser doesn't support geolocation it will show an error
-      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
-        infoWindow.setPosition(pos);
-        infoWindow.setContent(browserHasGeolocation ?
-                              'Error: The Geolocation service failed.' :
-                              'Error: Your browser doesn\'t support geolocation.');
-      }
+  ref.on('value', function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var userData = childSnapshot.val();
+      // console.log('user: ', userData.user);
+      // console.log('password: ', userData.password);
+      if (userInput === userData.user) {
 
-        // Display Transit layer for LA bus route
-        var transitLayer = new google.maps.TransitLayer();
-        transitLayer.setMap(map);
+        thisUserValid = true;
+        userValid = true;
+        console.log('this user valid?', thisUserValid);
+
+        if (passwordInput === userData.password) {
+            passwordValid = true;
+            thisPasswordValid = true;
+            console.log('this password valid? ', thisPasswordValid);
+        } else {
+            thisPasswordValid = false;
+            console.log('this password valid? ', thisPasswordValid);
+        }
+      } else {
+        thisUserValid = false;
+        thisPasswordValid = null;
+        console.log('this user valid?', thisUserValid);
+        console.log('this password valid? ', thisPasswordValid)
+      }  
+    });
+      // alert('check user and password now'); 
+      console.log('--------------------------');
+      console.log('userValid: ', userValid);
+      console.log('passwordValid: ', passwordValid);
+      if (userValid && passwordValid) {
+        userOK = true;
+        console.log('User OK?', userOK);
+        console.log('--------------------------');
+        return true;
       }
+      else {
+        userOK = false;
+        console.log('User OK?', userOK);
+        console.log('--------------------------');
+        return false;
+      }
+  });
+
+})
+
+//hide the dropdown menu 
+$("#dropdown1").hide();
+    $("#dropdown2").hide();
+    $("#soundDropdown").hide();    
+
+
+
+
+  
 
   
 
