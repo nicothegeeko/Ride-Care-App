a9f9fcbe0249b57c05eb4d064836694044e77bf3
Nico Flicker <Nico@Nicos-MacBook-Air.local>
2017-03-27 22:06:21 -0700

    adding updated google maps api

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,18 @@
+  //queryURL for metro API ROUTES
+function busRoute() {
+  var queryRoute = "https://api.metro.net/agencies/lametro/routes/180/";
+    $.get(queryRoute, function(data, success, type) {
+      console.log(data, 'data,!!!!!!!!!!!!!!!!!', success, type)
+    })
+     .success(function(response) {
+      console.log('123412341',response);
+    }).error(function(err) {
+      console.log('lskfjlksdjf', err)
+    })
+  
+  
+}
+
 var config = {
     apiKey: "AIzaSyBNKDQxtZjk6S7NwtlK0xgNT64ZDh3nx9s",
     authDomain: "ride-care-7c85d.firebaseapp.com",
@@ -123,113 +138,45 @@ var routeData;  // defined for global scope
 var validRoute = ['2', '92', '180', '780'];
 var validRouteSet = new Set(validRoute);
 
-function initMap() {
-  var latlng = new google.maps.LatLng(34.0397099,-118.55429);
-  var settings = {
-    zoom: 15,
-    center: latlng,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-  var map = new google.maps.Map(document.getElementById('map'), settings)
-}
+// Initiate Map to display google maps
+      function initMap() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          zoom: 13,
+          center: {lat: 34.052235, lng: -118.243683}
+        });
 
-// go to the LA Metro API to get bus route information
-function businfo(route) {
-  var baseurl = 'http://api.metro.net/agencies/lametro/routes/';
-  var queryURL = baseurl + route + '/sequence';
-  console.log("LA Metro API request: ", queryURL);
-  var stop = [];
-  var latitude = [];
-  var longitude = [];
+        var infoWindow = new google.maps.InfoWindow({map: map});
 
-  $.ajax({
-    url: queryURL,
-    method: "GET"
-  }).done(function(response) {
-    for (var i = 0; i < response.items.length; i++) {
-      stop.push(response.items[i].display_name);
-      latitude.push(response.items[i].latitude);
-      longitude.push(response.items[i].longitude);
-    };
-  }); 
-  var thisRouteData = {'stop': stop, 'latitude': latitude, 'longitude': longitude};  
-return thisRouteData;
-};  // end of function businfo 
 
-var routeSelect = document.getElementById("routeSelect");
-var route;
+        // Condition to grab user  geolocation.
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
 
-// must select route for anything else to happen
-$('#routeSelect').change(function routeChange() {  
-  var selectedOptionIndex = routeSelect.options['selectedIndex'];
-  if (selectedOptionIndex > 0) {
-    route = routeSelect.options[routeSelect.options['selectedIndex']].value;
-  };
-
-// if-block for execution of the app update with route stops included
-// only go further in the app if one of the valid routes has been selected
-  if (validRouteSet.has(route)) {
-    // collect stop, latitude, and longitude arrays for this route
-    routeData = businfo(route);
-    console.log(routeData);
-    var stop = routeData.stop;
-    var latitude = routeData.latitude;
-    var longitude = routeData.longitude;
-
-    // jQuery work to do on the DOM
-    // NEED TO APPEND TO startSelect LIST OF OPTIONS ALL THE STOPS FOR SELECTED ROUTE 
-    // NEED TO APPEND TO endSelect LIST OF OPTIONS ALL THE STOPS FOR SELECTED ROUTE
-
-    // determine the bounding box for the map based on route latitude and longitude arrays
-    var minLatitude = Math.min(latitude);
-    var maxLatitude = Math.max(latitude);
-    var minLongitude = Math.min(longitude);
-    var maxLongitude = Math.max(longitude);
-
-    // redraw the map based on the bounding box and the stop points for the seleced route
-  } // end of major if-block for running of the app
-
-  // functions for working with sets
-// source: https://developer.mozilla.org/
-//         en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
-Set.prototype.isSuperset = function(subset) {
-    for (var elem of subset) {
-        if (!this.has(elem)) {
-            return false;
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          // Browser doesn't support Geolocation
+          handleLocationError(false, infoWindow, map.getCenter());
         }
-    }
-    return true;
-}
 
-Set.prototype.union = function(setB) {
-    var union = new Set(this);
-    for (var elem of setB) {
-        union.add(elem);
-    }
-    return union;
-}
+        // if browser doesn't support geolocation it will show an error
+      function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                              'Error: The Geolocation service failed.' :
+                              'Error: Your browser doesn\'t support geolocation.');
+      }
 
-Set.prototype.intersection = function(setB) {
-    var intersection = new Set();
-    for (var elem of setB) {
-        if (this.has(elem)) {
-            intersection.add(elem);
-        }
-    }
-    return intersection;
-}
-
-Set.prototype.difference = function(setB) {
-    var difference = new Set(this);
-    for (var elem of setB) {
-        difference.delete(elem);
-    }
-    return difference;
-}
-
-});
-
-
-
-
+        // Display Transit layer for LA bus route
+        var transitLayer = new google.maps.TransitLayer();
+        transitLayer.setMap(map);
+      }
 
